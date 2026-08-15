c1cf339dc3c0f1fcae9087defcb8cd45a070ad21
j5zrb8m27c-bit <j5zrb8m27c@privaterelay.appleid.com>
2026-08-15 15:01:25 -0700

    Add V1.6.1 artifact: playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('@playwright/test');
+module.exports = defineConfig({
+  testDir: './tests',
+  use: { baseURL: 'http://127.0.0.1:8765' },
+  webServer: { command: 'python -m http.server 8765', port: 8765, reuseExistingServer: true },
+  reporter: 'line'
+});
+
